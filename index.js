a4f623efacc86574ec7ec61d2835470d8c00862c
Andrew Udvare <audvare@gmail.com>
2020-03-01 03:03:38 -0500

    index: comment out example strings

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const bnf = fs.readFileSync("./sel.jison", "utf8");
 // @ts-ignore
 const parser = new Parser(bnf);
 
-console.log(
-  parser.parse(`-sel:all,
-                +sel:((multi|stereo|mono)&favlang),
-                -sel:mvcvideo,
-                +sel:subtitle,
-                =100:all,
-                -10:favlang`)
-);
-console.log("");
-console.log(
-  parser.parse(
-    "-sel:all,+sel:(favlang|nolang),-sel:mvcvideo,=100:all,-10:favlang"
-  )
-);
+// console.log(
+//   parser.parse(`-sel:all,
+//                 +sel:((multi|stereo|mono)&favlang),
+//                 -sel:mvcvideo,
+//                 +sel:subtitle,
+//                 =100:all,
+//                 -10:favlang`)
+// );
+// console.log("");
+// console.log(
+//   parser.parse(
+//     "-sel:all,+sel:(favlang|nolang),-sel:mvcvideo,=100:all,-10:favlang"
+//   )
+// );
 
 module.exports = parser;
